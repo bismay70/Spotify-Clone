71127a020d252881ced08b3fce0d93438b153a46
bismay70 <samalbismay5@gmail.com>
2025-08-30 19:51:47 +0530

    ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import Sidebar from "./components/Sidebar"
+import Player from "./components/Player"
+import Display from "./components/Display"
+import { useContext } from "react"
+import { PlayerContext } from "./context/PlayerContext"
 
 
 function App() {
 
+  const {audioRef} = useContext(PlayerContext);
+
   return (
-    <>
-    
-    </>
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar/>
+        <Display/>
+      </div>
+      <Player/>
+      <audio ref={audioRef} preload="auto"></audio>
+    </div>
   )
 }
 
