35325820efc61982a2f4e66f5f310f27825b53c4
bismay70 <samalbismay5@gmail.com>
2026-01-25 23:46:54 +0530

    album controller

--- backend/src/routes/albumRoute.js
+++ backend/src/routes/albumRoute.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import {addAlbum,listAlbum,removeAlbum} from '../controllers/albumController.js'
+import { addAlbum, listAlbum, removeAlbum } from '../controllers/albumController.js'
 
 import upload from '../middleware/multer.js'
 
 const albumRouter = express.Router()
 
-albumRouter.post('/add',upload.single(['image']),addAlbum)
-albumRouter.get('/list',listAlbum)
-albumRouter.delete('/remove/',removeAlbum)
+albumRouter.post('/add', upload.single('image'), addAlbum)
+albumRouter.get('/list', listAlbum)
+albumRouter.post('/remove', removeAlbum)
 
 export default albumRouter
