cf3456961718ed78246bb5a6fc0c4dc2840c3aa1
bismay70 <samalbismay5@gmail.com>
2026-05-06 19:16:18 +0530

    song added and monog local changes

--- backend/src/config/mongodb.js
+++ backend/src/config/mongodb.js
@@ -4,6 +4,11 @@ const connectDB =  async () => {
     mongoose.connection.on('connected',()=>{
         console.log("connection established")
     })
+    
+    // PREVIOUS: MongoDB Atlas Connection (commented out due to timeout issues)
+    // const mongoUri = "mongodb+srv://username:password@cluster-name.mongodb.net/spotify?retryWrites=true&w=majority"
+    
+    // CURRENT: Local MongoDB Connection
     const mongoUri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/spotify"
     await mongoose.connect(mongoUri)
 }
